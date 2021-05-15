84d3b9178929f79c13ccc0c5aa6ea9aa4e484176
Елена Козловская <kozyulskaya>
2021-05-15 18:19:08 +0700

    a

---
+++ 1 lab.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+
+std::string crypt(const std::string& text) {
+    std::string cyphertext;
+
+    for (char c : text) {
+        cyphertext += char(24 - (c - 'a') + 1) + 'a';
+    }
+
+    return cyphertext;
+}
+
+int main() {
+    std::string text;
+    std::getline(std::cin, text);
+    std::string crypted = crypt(text);
+    std::cout << "Crypted text:       " << crypted << std::endl;
+    std::cout << "Twice crypted text: " << crypt(crypted) << std::endl;
+    return 0;
+}
